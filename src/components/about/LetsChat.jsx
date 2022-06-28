9e2f8b4b1f26205e64f7155b72d1c29c3c8168a2
kcblaq <kcblack22@gmail.com>
2022-06-28 17:31:12 +0100

    Built

--- src/components/about/LetsChat.jsx
+++ src/components/about/LetsChat.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './styles.module.css';
-import { Box, Typography, Button } from '@mui/material';
+import { Box, Typography, Button, Link } from '@mui/material';
 
 function LetsChat() {
 	return (
